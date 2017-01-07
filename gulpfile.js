e47089995e7f78fd43a7c9a9091c3b826bd6e103
Christophe MEJEAN <christophemma@live.fr>
2017-01-07 15:56:03 +0100

    Ajout html5 basic layout SASS

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('styles', function() {
+    gulp.src('Html5LayoutSass/sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('Html5LayoutSass/./css/'))
+});
+
+//Watch task
+gulp.task('default',function() {
+    gulp.watch('Html5LayoutSass/sass/**/*.scss',['styles']);
+});
