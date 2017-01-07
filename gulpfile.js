9e5a54627a45fc700c16a90e0319065036c098fc
Christophe MEJEAN <christophemma@live.fr>
2017-01-07 23:50:18 +0100

    exec styles dans default gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,13 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
-gulp.task('styles', function() {
+gulp.task('styles', function () {
     gulp.src('Html5LayoutSass/sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('Html5LayoutSass/./css/'))
 });
 
 //Watch task
-gulp.task('default',function() {
-    gulp.watch('Html5LayoutSass/sass/**/*.scss',['styles']);
+gulp.task('default', ['styles'], function () {
+    gulp.watch('Html5LayoutSass/sass/**/*.scss', ['styles']);
 });
