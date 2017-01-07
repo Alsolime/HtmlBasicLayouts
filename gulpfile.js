8c8c0186c97d81d849812ba7b2c8b16e71942e93
Christophe MEJEAN <christophemma@live.fr>
2017-01-08 00:51:35 +0100

    Configuration tache webserver

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,22 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var webserver = require('gulp-webserver');
+
 
 gulp.task('styles', function () {
     gulp.src('Html5LayoutSass/sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('Html5LayoutSass/./css/'))
+});
+
+//Server
+gulp.task('webserver', function () {
+    gulp.src('Html5LayoutSass')
+        .pipe(webserver({
+            livereload: true,
+            open: true
+        }));
 });
 
 //Watch task
